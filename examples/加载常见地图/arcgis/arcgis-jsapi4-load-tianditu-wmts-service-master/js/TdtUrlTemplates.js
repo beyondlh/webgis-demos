8291e6bdce06118366147f991558cdde4d87f114
leihao <abclh2580@126.com>
2022-05-20 12:41:31 +0800

    arcgis api 叠加不同坐标系底图

---
+++ examples/加载常见地图/arcgis/arcgis-jsapi4-load-tianditu-wmts-service-master/js/TdtUrlTemplates.js
@@ -0,0 +1,6 @@
+define(function() {
+  var urlTemplatesObject = {
+    TDT: "http://222.85.34.130:8081/geoesb/proxy/ff56b1eac03047d193db62fd16db9caa/072a3bd4b19c459288df5a672f02f32e?service=WMTS&request=GetTile&version=1.0.0&style=default&tilematrixSet=CustomCRS4490ScalePDS_DLG_2019&format=image%2Fpng&width=256&height=256&layer=PDS_DLG_2019&tilematrix={level}&tilerow={row}&tilecol={col}"
+  };
+  return urlTemplatesObject;
+});
